cd119ce56b3efecaa185a539f04c09d10cc73707
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-08-09 18:37:44 +0530

    Setup done

--- whatsapp/settings.py
+++ whatsapp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'chatapp.apps.ChatappConfig',
+    # Libraries
+    'channels',
 ]
 
 MIDDLEWARE = [
